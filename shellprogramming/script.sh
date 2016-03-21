9a321715ac70621dcccb50c59ea3044acc6c2647
Xiang Wang <ramwin@qq.com>
2016-03-21 23:25:20 +0800

    判断语句

--- shellprogramming/script.sh
+++ shellprogramming/script.sh
@@ -3,6 +3,7 @@
 
 echo "Hello, world."
 
+# 判断
 if [ -e basic02 ] # 是否存在basic02这个文件
 # if test -e . # [ ] 和 test 一样
 then
@@ -10,3 +11,16 @@ then
 else
     echo "不存在basic02"
 fi
+
+# 循环
+for fn in *; do
+    echo "$fn"
+done
+
+for fn in tom dick harry; do
+    echo "$fn"
+done
+
+ls -1 | while read fn; do
+    echo "$fn"
+done
