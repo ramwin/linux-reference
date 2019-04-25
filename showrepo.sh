7198b23554f106af471c322d70c778fe4622d530
Xiang Wang <ramwin@qq.com>
2019-04-25 17:57:13 +0800

    改动

--- showrepo.sh
+++ showrepo.sh
@@ -3,13 +3,25 @@
 
 
 checkStatus() {
+    # 这个只会看暂存区，工作区是否有编辑
     status=`git status --porcelain` 
     if [[ $status ]]; then
-        echo "有编辑"
+        echo "当前暂存区或者工作区有改动"
         echo $status
     else
         echo "没有编辑"
     fi
 }
 
+pull() {
+    local result=`git pull -q origin master`
+    echo $result
+    if [[ $result ]]; then
+        echo "拉取到了新代码"
+    else
+        echo "没有拉取到新代码"
+    fi
+}
+
 checkStatus
+pull
