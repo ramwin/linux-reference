61be42c3b0c8d2b1a84d7ca06b1a75f26f12bcaa
Xiang Wang <ramwin@qq.com>
2019-04-25 18:26:38 +0800

    比较origin

--- showrepo.sh
+++ showrepo.sh
@@ -8,25 +8,22 @@ checkStatus() {
     if [[ $status ]]; then
         echo "当前暂存区或者工作区有改动"
         echo $status
-    else
-        echo "没有编辑"
+    # else
+    #     echo "没有编辑"
     fi
 }
 
 pull() {
-    echo "pull"
     local result=`git pull -q origin master`
     echo $result
     if [[ $result ]]; then
         echo "拉取到了新代码"
-    else
-        echo "没有拉取到新代码"
+    # else
+    #     echo "没有拉取到新代码"
     fi
-    echo "pull结束"
 }
 
 diffCheck() {
-    echo "检查"
     local origindiff=`git diff --exit-code --shortstat origin/master`
     if [[ $origindiff ]]; then
         echo $origindiff
