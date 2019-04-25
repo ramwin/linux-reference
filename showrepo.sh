5584845d86b21ef37dd52432d146f6559e336f95
Xiang Wang <ramwin@qq.com>
2019-04-25 18:25:35 +0800

    不一样

--- showrepo.sh
+++ showrepo.sh
@@ -8,10 +8,13 @@ checkStatus() {
     if [[ $status ]]; then
         echo "当前暂存区或者工作区有改动"
         echo $status
+    else
+        echo "没有编辑"
     fi
 }
 
 pull() {
+    echo "pull"
     local result=`git pull -q origin master`
     echo $result
     if [[ $result ]]; then
@@ -19,7 +22,27 @@ pull() {
     else
         echo "没有拉取到新代码"
     fi
+    echo "pull结束"
+}
+
+diffCheck() {
+    echo "检查"
+    local origindiff=`git diff --exit-code --shortstat origin/master`
+    if [[ $origindiff ]]; then
+        echo $origindiff
+        echo "和origin不一样"
+    else
+        echo "和origin一样"
+    fi
+    local WXdiff=`git diff --exit-code --shortstat WX/master`
+    if [[ $WXdiff ]]; then
+        echo $WXdiff
+        echo "和WX不一样"
+    else
+        echo "和WX一样"
+    fi
 }
 
 checkStatus
 pull
+diffCheck
