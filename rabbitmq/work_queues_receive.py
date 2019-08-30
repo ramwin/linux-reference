4170b769068cd9c0326018b48bdc07859c5c0932
Xiang Wang <ramwin@qq.com>
2019-08-30 17:31:35 +0800

    rabbit的持久化

--- rabbitmq/work_queues_receive.py
+++ rabbitmq/work_queues_receive.py
@@ -11,12 +11,55 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue="hello")  # 万一send.py后启动,这样就没有hello这个channel了. 所以安全起见还是先创建一个queue再说
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    time.sleep(body.count(b"."))
-    print(" [x] Done")
+# 这种崩溃了就会丢失任务
+def run1():
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        time.sleep(body.count(b"."))
+        print(" [x] Done")
 
 
-channel.basic_consume(queue="hello", auto_ack=True, on_message_callback=callback)
-print(" [*] Waiting for messages. To exit press CTRL+C")
-channel.start_consuming()
+    channel.basic_consume(queue="hello", auto_ack=True, on_message_callback=callback)
+    print(" [*] Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+
+def run2():
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        time.sleep(body.count(b"."))
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+
+
+    channel.basic_consume(queue="hello", on_message_callback=callback)
+    print(" [*] Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+
+def run3():
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        time.sleep(body.count(b"."))
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+
+
+    channel.basic_consume(queue="task_queue", on_message_callback=callback)
+    print(" [*] Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+
+def run4():
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        time.sleep(body.count(b"."))
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue="task_queue", on_message_callback=callback)
+    print(" [*] Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+    run4()
--- rabbitmq/work_queues_send.py
+++ rabbitmq/work_queues_send.py
@@ -7,9 +7,22 @@ import pika
 import sys
 
 
-message = " ".join(sys.argv[1:]) or "Hello World!"
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue="hello")
-channel.basic_publish(exchange="", routing_key="hello", body=message)
-connection.close()
+def run1():
+    message = " ".join(sys.argv[1:]) or "Hello World!"
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue="hello")
+    channel.basic_publish(exchange="", routing_key="hello", body=message)
+    connection.close()
+
+def run3():
+    message = " ".join(sys.argv[1:]) or "Hello World!"
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue="task_queue", durable=True)
+    channel.basic_publish(exchange="", routing_key="task_queue", body=message)
+    connection.close()
+
+
+if __name__ == "__main__":
+    run3()
