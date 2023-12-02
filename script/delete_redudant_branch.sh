024aa721f79de5b486af745c17839b03b9d7889b
Xiang Wang <ramwin@qq.com>
2023-12-02 17:10:45 +0800

    soft delete

--- script/delete_redudant_branch.sh
+++ script/delete_redudant_branch.sh
@@ -4,8 +4,9 @@
 set -e
 
 rebase_to_master.sh
+git checkout master
 
 for branch_name in $( git branch --format="%(refname:short)" --merged | grep -v 'master' ); do
     echo "处理分支:" $branch_name
-    git branch -D $branch_name
+    git branch -d $branch_name
 done
