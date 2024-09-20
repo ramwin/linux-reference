c697ca99cce4fff3d793d805e9902a5848ba16e2
Xiang Wang <ramwin@qq.com>
2024-09-20 22:12:51 +0800

    feat: nothing

--- crontab.sh
+++ crontab.sh
@@ -2,8 +2,5 @@
 # Xiang Wang(ramwin@qq.com)
 
 
-/home/wangx/github/secret/shadowsocks/run_sslocal.sh \
-& syncthing \
-& (cd /home/wangx/github/linux-reference/ && sh run_server.sh) \
-& (cd /home/wangx/github/python-reference/ && sh run_server.sh) \
-& (cd /home/wangx/github/django-reference/ && sh run_server.sh)
+@reboot syncthing --no-browser
+@reboot /home/wangx/github/secret/shadowsocks/run_sslocal.sh
