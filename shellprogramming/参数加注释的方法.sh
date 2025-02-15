2beefe7065eaf7cf0defa517e81372721e25f0e1
Xiang Wang <ramwin@qq.com>
2025-02-15 15:11:20 +0800

    fix: 函数添加注释的方法

---
+++ shellprogramming/参数加注释的方法.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Xiang Wang(ramwin@qq.com)
+#
+
+
+# 方法: https://stackoverflow.com/questions/9522631/how-to-put-a-line-comment-for-a-multi-line-command
+
+echo_args=(
+    "123"
+    "3425"
+)
+echo "${echo_args[@]}"
