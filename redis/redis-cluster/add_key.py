d88bbe532b65a946972d9747b4ef46d42cadc5f0
Xiang Wang <ramwin@qq.com>
2025-05-05 23:59:02 +0800

    fix: 不加prefix

--- redis/redis-cluster/add_key.py
+++ redis/redis-cluster/add_key.py
@@ -7,11 +7,12 @@ import redis
 import string
 
 
-prefix = str(time.time())
+# prefix = str(time.time())
 r = redis.RedisCluster(host="localhost", port=7000)
 
 for i in string.ascii_letters:
     r.set(i, i)
 
 for i in range(10000):
-    r.set(prefix + str(i), i)
+    # r.set(prefix + str(i), i)
+    r.set(str(i), i)
