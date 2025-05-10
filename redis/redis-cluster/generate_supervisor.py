7b9c6fb8e61d05abb0b34a0fa786d4c3991f7ffa
Xiang Wang <ramwin@qq.com>
2025-05-10 17:12:33 +0800

    feat: 目录配置

--- redis/redis-cluster/generate_supervisor.py
+++ redis/redis-cluster/generate_supervisor.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
-import jinjia2
+from pathlib import Path
+from jinja2 import Template
 
+
+DIRECTORY="/home/wangx/github/linux-reference/redis/redis-cluster/cluster-test"
 
 template = Template(
     "[program:redis_cluster_{{port}}]\n"
-    "command=sh run_server.sh\n"
-    "directory=/home/wangx/github/linux-reference\n"
+    "command=redis-server redis_cluster.conf\n"
+    "directory={{DIRECTORY}}/{{port}}\n"
     "stdout_logfile_maxbytes=4MB\n"
     "stdout_logfile_backups=20\n"
     "stderr_logfile_maxbytes=4MB\n"
@@ -19,7 +22,12 @@ template = Template(
     "startsecs=20\n"
     "redirect_stderr=false\n"
     "user=wangx\n"
-    "environment=PATH="/home/wangx/venv/bin/:/home/wangx/.local/bin:/home/wangx/node/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin\n"
-    "stdout_logfile=/home/wangx/github/linux-reference/stdout.log\n"
-    "stderr_logfile=/home/wangx/github/linux-reference/stderr.log\n"
+    'environment=PATH="/home/wangx/venv/bin/:/home/wangx/.local/bin:/home/wangx/node/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"\n'
+    "stdout_logfile={{DIRECTORY}}/stdout.log\n"
+    "stderr_logfile={{DIRECTORY}}/stderr.log\n\n\n"
 )
+
+with open(Path("supervisor_redis_cluster.ini"), "w") as f:
+    for port in [7000, 7001, 7002]:
+        f.write(template.render({"port": port, "DIRECTORY": DIRECTORY}))
+
