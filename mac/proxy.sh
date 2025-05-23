fab098c128bd9e40f549593125adb1b3acb73174
Xiang Wang <ramwin@qq.com>
2025-05-23 21:47:24 +0800

    feat: proxy

---
+++ mac/proxy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Xiang Wang(ramwin@qq.com)
+
+export https_proxy=http://localhost:1087
+export HTTP_PROXY=http://localhost:1087
+export HTTPS_PROXY=http://localhost:1087
+export no_http_proxy="localhost;ramwin.com;*.cn"
+export no_https_proxy="localhost;ramwin.com;*.cn"
