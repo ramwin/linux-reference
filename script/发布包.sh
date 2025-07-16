7eb191474be9a0f73fcc72490433f54f98ece5e2
Xiang Wang <ramwin@qq.com>
2025-07-16 21:02:22 +0800

    feat: 发布包

--- script/发布包.sh
+++ script/发布包.sh
@@ -2,8 +2,11 @@
 # Xiang Wang(ramwin@qq.com)
 
 rm -rf dist/*
-hatch build
-hatch publish
+# hatch build
+# hatch publish
+#
+python3 -m build
+twine upload dist/*
 
 # python3 setup.py sdist bdist_wheel
 # twine upload -r dist/*
